d789cda0e5c6f35a55567db3335e0106325df3f1
luizQF <luizfqueiroz1811@gmail.com>
2024-12-15 19:59:08 -0300

    .

--- exercicios/exercicio-15(data de nascimento)/script.js
+++ exercicios/exercicio-15(data de nascimento)/script.js
@@ -8,12 +8,12 @@ function clicar(){//a função não pode ter o mesmo nome da ação
         alert('[ERROR] Verifique os dados e tente novamente.')    
     }else {
         var idade = ano - nasceu.value
-        var img = document.createElement('img')
-        img.setAttribute('id', 'img')
-        var sex = document.getElementsByName('rsex') 
+        var img = document.createElement('img')//cria tag img no documento
+        img.setAttribute('id', 'img')//id="img"
+        var sex = document.getElementsByName('rsex') //tras os dois radios para o script
         var genero = ''
 
-        if(sex[0].checked){
+        if(sex[0].checked){//se o primeiro radio estiver marcado
             genero = 'Homem'
             if(idade < 10){
                 img.setAttribute('src', 'imagens/criançah.jpg')
@@ -24,12 +24,12 @@ function clicar(){//a função não pode ter o mesmo nome da ação
             }else{
                 img.setAttribute('src', 'imagens/idoso.jpg')
             }
-        }else if (sex[1].checked){
+        }else if (sex[1].checked){//se o segundo estiver marcado
             genero = 'Mulher'
             if(idade < 10){
                 img.setAttribute('src', 'imagens/criançam.jpg')
             }else if (idade < 21){
-                img.setAttribute('src', 'imagens/jovemm.pjg')
+                img.setAttribute('src', 'imagens/jovemm.jpg')
             }else if(idade < 50){
                 img.setAttribute('src', 'imagens/mulher.jpg')
             }else{
@@ -38,7 +38,7 @@ function clicar(){//a função não pode ter o mesmo nome da ação
         }
         
         res.innerHTML = `Detectamos um ${genero} de ${idade} Anos`
-        res.appendChild(img)
+        res.appendChild(img)//reproduz a imagem abaixo da var res
         res.style.textAlign = 'center'
     }
    
